14505691bb7cf61c8423314375255b8ced4d0ffd
dfcc24 <dfcc24@gmail.com>
2025-05-29 15:20:31 -0500

    creacion de compornetsr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/headeer/headeer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component'; // Assuming this is the correct import path for the CarruselComponent
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , HeaderComponent],
+  imports: [RouterOutlet , HeaderComponent , FooterComponent , CarruselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
