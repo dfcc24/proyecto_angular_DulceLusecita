a35786e8ae08bea1c26a52c3f79f6d27555b5e16
dfcc24 <dfcc24@gmail.com>
2025-05-29 15:04:04 -0500

    correccion de headeer por header

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeadeerComponent } from './headeer.component';
+import { HeaderComponent } from './header.component';
 
-describe('HeadeerComponent', () => {
-  let component: HeadeerComponent;
-  let fixture: ComponentFixture<HeadeerComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeadeerComponent]
+      imports: [HeaderComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HeadeerComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
